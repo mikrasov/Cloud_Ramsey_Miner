b953516b1b425973c7fc3543be69a48450088cc4
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2014-04-27 18:53:52 -0700

    Fix Miner Code
    Fix Miner code

--- src/com/mikrasov/cloudmine/Miner.java
+++ src/com/mikrasov/cloudmine/Miner.java
@@ -2,19 +2,14 @@ package com.mikrasov.cloudmine;
 
 public class Miner {
 	
-	public Miner() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public void mine(){
 
 		//start with graph size 8
-		Graph g = new Graph(8);
+		Graph g = Graph.generateRandom(8); //= Graph.generateRandom(8);
 		TabooList taboo = new TabooList();
-		
 
 		// while we do not have a publishable result
-		while(g.size() < 102)
+		while(g.size() < 80)
 		{
 			//find out how we are doing
 			int count = g.cliqueCount();
@@ -22,10 +17,10 @@ public class Miner {
 			//if we have a counter example
 			if(count == 0)
 			{
-				System.out.println("Eureka!  Counter-example found!\n"+g);
+				System.out.println(">>Counter-example found on "+g.size()+"!");
 	
 				//make a new graph one size bigger 
-				g = g.extend();
+				g = g.extendRandom();
 				
 				// reset the taboo list for the new graph
 				taboo = new TabooList();
@@ -44,9 +39,10 @@ public class Miner {
 			 * only need to work with upper triangle of matrix =>
 			 * notice the indices
 			 */
+			
 			int best_count = Integer.MAX_VALUE;
-			int best_i=0;
-			int best_j=0;
+			int best_i=-1;
+			int best_j=-1;
 			for(int i=0; i < g.size(); i++)
 			{
 				for(int j=i+1; j < g.size(); j++)
@@ -55,23 +51,30 @@ public class Miner {
 					g.flip(i,j);
 					count = g.cliqueCount();
 
+					if(count == 0) break;
+					
 					// is it better and the i,j,count not taboo?
-					if((count < best_count) && 
-						taboo.contains(i, j));
+					if( count < best_count && !taboo.contains(i, j))
 					{
 						best_count = count;
 						best_i = i;
-						best_j = j;
+						best_j = j;	
 					}
 
 					//flip it back
 					g.flip(i,j);
+					
 				}
+				
+				if(count == 0) break;
 			}
+			
+			if(count == 0) continue;
 
 			if(best_count == Integer.MAX_VALUE) {
 				System.out.println("no best edge found, terminating");
-				System.exit(1);
+				g.flipRandom();
+				continue;
 			}
 			
 			// keep the best flip we saw
@@ -86,12 +89,13 @@ public class Miner {
 //			FIFOInsertEdgeCount(taboo_list,best_i,best_j,count);
 
 			System.out.println("size: "+g.size()+"\t"
-					+ "best_size: "+best_count+"\t"
+					+ "best_count: "+best_count+"\t"
 					+ "best edge: ("+best_i+","+best_j+")\t"
 					+ "color: "+ (g.get(best_i, best_j)?1:0)
 					);
 			// rinse and repeat
 		}
+		
 	}
 	
 	public static void main(String[] args) {
--- src/com/mikrasov/cloudmine/TabooList.java
+++ src/com/mikrasov/cloudmine/TabooList.java
@@ -5,13 +5,17 @@ import java.util.HashSet;
 public class TabooList {
 
 	private HashSet<Integer> list = new HashSet<>();
-	
+
 	public void add(int i, int j){
 		list.add(hash(i,j));
 	}
 
 	public boolean contains(int i, int j){
 		return list.contains(hash(i,j));
+	}
+	
+	public int size(){
+		return list.size();
 	}
 	
 	protected int hash(int i, int j){
