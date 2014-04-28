0c96cc46ae139fa504f357598997ccc4d31f2222
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2014-04-27 18:03:49 -0700

    Random Flips
    Added documentations.
    Added ability to flip random node.

--- src/com/mikrasov/cloudmine/Graph.java
+++ src/com/mikrasov/cloudmine/Graph.java
@@ -1,19 +1,20 @@
 package com.mikrasov.cloudmine;
 
 import java.util.BitSet;
+import java.util.Random;
 
-/**
- * Based on BitMatrix
- * @author Timothy A. Budd
- * @version 1.1 September 1999
- * @see jds.Collection
- */
 
 public class Graph {
-	private BitSet [ ] rows;
 	private static final int SUB_GRAPH_SIZE = 6;
+	private static final Random rnd = new Random();
 	
+	private BitSet [ ] rows;
 	private int size; 
+	
+	/**
+	 * Create new matrix based Graph representation (zero filled)
+	 * @param size of graph to create
+	 */
 	public Graph (int size) {
 		this.size = size;
 		rows = new BitSet[size];
@@ -21,38 +22,57 @@ public class Graph {
 			rows[i] = new  BitSet(size);
 	}
 
-
-	public boolean get(int i, int j) { return rows[i].get(j); }
-
-	public void set(int i, int j, boolean value) { 
-		if(value)
-			rows[i].set(j);
-		else
-			rows[i].clear(j);
-	}
-	
-	public void flip(int i, int j) { 
-		rows[i].flip(j);
-	}
-	
-	public Graph extend(){
-		int newSize = size +1;
-		Graph graph = new Graph(newSize);
-		
-		for(int i=0; i < size; i++) {
-			for(int j=0; j < size; j++) {
-				graph.set(i, j, get(i,j));
-			}
-		}
-		
-		return graph;
-	}
-	
-	
+	/**
+	 * Get graph size
+	 * @return number of nodes in graph
+	 */
 	public int size(){
 		return size;
 	}
+
+	/**
+	 * Get value of matrix represented edge
+	 * @param row 
+	 * @param col
+	 * @return value
+	 */
+	public boolean get(int row, int col) { return rows[row].get(col); }
+
+	/**
+	 * Set value of edge
+	 * @param row
+	 * @param col
+	 * @param value
+	 */
+	public void set(int row, int col, boolean value) { 
+		if(value)
+			rows[row].set(col);
+		else
+			rows[row].clear(col);
+	}
 	
+	/**
+	 * Flip value of edge
+	 * @param row
+	 * @param col
+	 */
+	public void flip(int row, int col) { 
+		rows[row].flip(col);
+	}
+	
+	/**
+	 * Flip a random edge (in the upper triangle)
+	 */
+	public void flipRandom(){
+		int row = rnd.nextInt(size-1);
+		int col = row + 1+ rnd.nextInt(size-row-1);
+		flip(row,col);
+	}
+	
+	/**
+	 * Get clique count
+	 * @return clique count
+	 */
 	public int cliqueCount() {
 	    int count=0;
 	    
@@ -97,24 +117,85 @@ public class Graph {
 	    return count;
 	}
 	
-	public static Graph generateRandom(int size){
-		Graph g = new Graph(size);
+	/**
+	 * Extend the graph by adding a node (increasing the size by 1).
+	 * The new edges are zero filled.
+	 * @return extended graph
+	 */
+	public Graph extend(){
+		int newSize = size +1;
+		Graph graph = new Graph(newSize);
 		
-		for(int i=0; i < size; i++) {
-			for(int j=0; j < size; j++) {
-				if(Math.random() < 0.5) 
-					g.set(i,j, true);
+		for(int row=0; row < size; row++) {
+			for(int col=0; col < size; col++) {
+				graph.set(row, col, get(row,col));
 			}
 		}
 		
+		return graph;
+	}
+	
+	/**
+	 * Extend the graph by adding a node (increasing the size by 1).
+	 * The new edges are filled with random values.
+	 * @return extended graph
+	 */
+	public Graph extendRandom(){
+		Graph graph = extend();
+		
+		for(int row=0; row < graph.size()-1; row++) {
+			if(rnd.nextBoolean()) 
+				graph.set(row, graph.size()-1, true);
+		}
+		return graph;
+	}
+	
+	/**
+	 * Mirror upper triangle to lower triangle
+	 * @return mirrored graph
+	 */
+	public Graph mirror(){
+		Graph graph = new Graph(size);
+		
+		for(int row=0; row < size; row++) {
+			for(int col=row+1; col < size; col++) {
+				if(this.get(row, col)) {
+					graph.set(row,col, true);
+					graph.set(col,row, true);
+				}
+			}
+		}
+		return graph;
+	}
+	
+	/**
+	 * Generate random graph as upper triangular matrix
+	 * @param size of graph to generate
+	 * @return generated graph
+	 */
+	public static Graph generateRandom(int size){
+		Graph g = new Graph(size);
+		
+		for(int row=0; row < size; row++) {
+			for(int col=row+1; col < size; col++) {
+				if(rnd.nextBoolean()) 
+					g.set(row,col, true);
+			}
+		}
 		return g;
 	}
 
-	public String asJSON(){
+	/**
+	 * Mirror upper triangular matrix, and encode it as a string
+	 * @return string
+	 */
+	public String encodeAsJsonValue(){
+		Graph graph = this.mirror();
+		
 		String out ="";
-		for(int i=0; i < size; i++) {
-			for(int j=0; j < size; j++) {
-				out+=  get(i,j)?1:0 ;
+		for(int row=0; row < size; row++) {
+			for(int col=0; col < size; col++) {
+				out+=  graph.get(row,col)?1:0 ;
 			}
 		}
 		return out;
@@ -123,10 +204,10 @@ public class Graph {
 	@Override
 	public String toString() {		
 		String out ="";
-		for(int i=0; i < size; i++) {
-			for(int j=0; j < size; j++)
+		for(int row=0; row < size; row++) {
+			for(int col=0; col < size; col++)
 			{
-				out+=  (get(i,j)?1:0) +" ";
+				out+=  (get(row,col)?1:0) +" ";
 			}
 			out += "\n";
 		}
